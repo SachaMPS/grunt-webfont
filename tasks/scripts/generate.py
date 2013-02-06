292ab6ec2b7e436947eb2510f9fd76938046fe21
SachaMPS <sacha@mllrsohn.com>
2013-02-06 12:18:32 +0100

    dont use mkeot

--- tasks/scripts/generate.py
+++ tasks/scripts/generate.py
@@ -83,7 +83,7 @@ if 'woff' in args.types:
 
 # EOT
 if 'eot' in args.types:
-	subprocess.call('mkeot ' + fontfile + '.ttf > ' + fontfile + '.eot', shell=True)
+	subprocess.call('ttf2eot ' + fontfile + '.ttf > ' + fontfile + '.eot', shell=True)
 
 # Hint the TTF file or delete it if not needed
 if 'ttf' in args.types:
